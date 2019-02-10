1cccf355a25477ac41571095465f78fe5f434603
Charles Hinson <cehinso17@gmail.com>
2019-02-10 22:38:12 +0800

    basic demo of random agent

--- ai/demo.py
+++ ai/demo.py
@@ -1,13 +1,19 @@
+
 import time
+from functools import partial
 
 from agents.random_agent import Random_Agent
 from actuators.actuator import Actuator
 
-test_actuator = Actuator(("North", "South", "East", "West"))
+test_actuator = Actuator([partial(print, "North"),
+                          partial(print, "South"),
+                          partial(print, "East"),
+                          partial(print, "West")])
+
 agent = Random_Agent(None, test_actuator)
 
 input("Press any key to continue...")
 while True:
     action = agent.next_action()
-    print(action)
-    time.sleep(2)
+    action()
+    time.sleep(1)
