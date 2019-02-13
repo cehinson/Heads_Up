ebd2cde20c68bc915d7188e6acb749a291fe3dd5
Charles Hinson <cehinso17@gmail.com>
2019-02-13 19:57:04 +0800

    Added TransparentWidget

---
+++ widgets/transparent_widget.py
@@ -0,0 +1,42 @@
+
+import sys
+
+from PySide2.QtWidgets import (
+    QApplication,
+    QVBoxLayout,
+    QWidget,
+)
+
+from PySide2.QtCore import (
+    Qt
+)
+
+
+class TransparentWidget(QWidget):
+    '''
+    A semi-transparent window
+    '''
+
+    def __init__(self, opacity=0.0, frameless=True):
+        QWidget.__init__(self)
+        # make the window transparent
+        self.setWindowOpacity(opacity)
+        # remove the top bar
+        if frameless:
+            self.setWindowFlag(Qt.FramelessWindowHint)
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    screen = app.primaryScreen()
+    size = screen.size()
+
+    widget = TransparentWidget()
+    widget.resize(size.width(), size.height())
+    widget.show()
+
+    sys.exit(app.exec_())
