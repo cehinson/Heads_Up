3041eec9bfb555e03c111dbf6ff6e6c7f2242ebd
Charles Hinson <cehinso17@gmail.com>
2019-02-14 18:58:45 +0800

    screen select widget almost done

--- widgets/screen_select_widget.py
+++ widgets/screen_select_widget.py
@@ -11,23 +11,59 @@ from PySide2.QtGui import (
 
 from PySide2.QtWidgets import (
     QRubberBand,
-    QApplication
+    QApplication,
+    QWidget,
+    QPushButton,
+    QVBoxLayout
 )
 
 from PySide2.QtCore import (
     Qt,
     QRect,
     QPoint,
-    QSize
+    QSize,
+    QObject,
+    Signal,
+    Slot,
 )
+
+
+class SelectScreenButton(QWidget):
+    def __init__(self):
+        super(SelectScreenButton, self).__init__()
+        # setup select screen widget
+        self.ssw = ScreenSelectWidget()
+        self.make_connection(self.ssw)
+        # setup button
+        self.select_area_button = QPushButton("Select Area")
+        self.select_area_button.clicked.connect(self.select_area)
+        # add to layout
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.select_area_button)
+
+    def select_area(self):
+        self.hide()
+        self.ssw.showMaximized()
+
+    @Slot(tuple)
+    def area_selected(self, rect):
+        self.ssw.hide()
+        print("Area selected")
+        print(rect)
+        self.show()
+
+    def make_connection(self, ssw_object):
+        ssw_object.areaSelected.connect(self.area_selected)
 
 
 class ScreenSelectWidget(TransparentWidget):
     '''A fully transparent widget used for selecting areas of a screen'''
 
-    def __init__(self, width, height):
+    # add a signal
+    areaSelected = Signal(tuple)
+
+    def __init__(self):
         super().__init__(opacity=0.50)
-        self.resize(width, height)
         # select area
         self.rubberband = QRubberBand(QRubberBand.Rectangle, self)
         # FIXME set the color
@@ -44,26 +80,24 @@ class ScreenSelectWidget(TransparentWidget):
         self.h = 0
 
     def mousePressEvent(self, event):
-        print("mousePressEvent")
         if event.button() == Qt.LeftButton:
             self.origin = QPoint(event.pos())
             self.rubberband.setGeometry(QRect(self.origin, QSize()))
             self.rubberband.show()
 
     def mouseMoveEvent(self, event):
-        print("mouseMoveEvent")
         if not self.origin.isNull():
             self.rubberband.setGeometry(
                 QRect(self.origin, event.pos()).normalized())
 
     def mouseReleaseEvent(self, event):
-        print("mouseReleaseEvent")
         if event.button() == Qt.LeftButton:
             self.x = self.rubberband.x()
             self.y = self.rubberband.y()
             self.w = self.rubberband.width()
             self.h = self.rubberband.height()
-            print(self.getCoords())
+            self.areaSelected.emit(self.getCoords())
+            self.rubberband.hide()
 
     def getCoords(self):
         return (self.x, self.y, self.w, self.h)
@@ -72,11 +106,7 @@ class ScreenSelectWidget(TransparentWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    screen = app.primaryScreen()
-    size = screen.size()
-
-    widget = ScreenSelectWidget()
-    widget.resize(size.width(), size.height())
+    widget = SelectScreenButton()
     widget.show()
 
     sys.exit(app.exec_())
