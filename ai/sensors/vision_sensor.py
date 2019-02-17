133408e78372b6fbf8b5b11790ba5945f60ff84b
Charles Hinson <cehinso17@gmail.com>
2019-02-17 17:19:28 +0800

    basic VisionSensor works

--- ai/sensors/vision_sensor.py
+++ ai/sensors/vision_sensor.py
@@ -1,11 +1,21 @@
 
-from sensors.sensor import Sensor
+from sensor import Sensor
 
-import PIL
+import pyautogui
 
 
 class VisionSensor(Sensor):
     '''Input from a region of the screen'''
 
     def __init__(self, rect):
-        raise NotImplementedError
+        self.rect = rect
+
+    def next_percept(self):
+        return pyautogui.screenshot(region=self.rect)
+
+
+if __name__ == '__main__':
+    vs = VisionSensor((0, 0, 300, 400))
+    im = vs.next_percept()
+    im.save('test.png')
+    im.show()
