5d8a966fbf5da9043897f29a2c5a75b19c3ba5b8
Charles Hinson <cehinso17@gmail.com>
2019-02-28 11:23:40 +0800

    basic keyboard sensor working

--- ai/sensors/keyboard_sensor.py
+++ ai/sensors/keyboard_sensor.py
@@ -8,8 +8,28 @@ class KeyboardSensor(Sensor):
     '''Monitor key presses '''
 
     def __init__(self):
-        super().__init__()
         self.events = deque()
+        # Collect events until released
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+            listener.join()
 
     def next_percept(self):
         return self.events.pop()
+
+    def on_press(self, key):
+        try:
+            print('alphanumeric key {0} pressed'.format(key.char))
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+
+    def on_release(self, key):
+        print('{0} released'.format(key))
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+
+if __name__ == '__main__':
+    mysensor = KeyboardSensor()
+    while True:
+        pass
