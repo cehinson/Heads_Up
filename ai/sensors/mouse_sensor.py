c173bdf7f3fa6f9d7a2963f2d3843a0bdd3f15c9
Charles Hinson <cehinso17@gmail.com>
2019-02-28 11:27:52 +0800

    Mouse sensor works

--- ai/sensors/mouse_sensor.py
+++ ai/sensors/mouse_sensor.py
@@ -8,8 +8,29 @@ class MouseSensor(Sensor):
     '''Monitor mouse events'''
 
     def __init__(self):
-        super().__init__()
         self.events = deque()
+        # Collect events until released
+        with mouse.Listener(on_move=self.on_move, on_click=self.on_click,
+                            on_scroll=self.on_scroll) as listener:
+            listener.join()
 
     def next_percept(self):
         return self.events.pop()
+
+    def on_move(self, x, y):
+        print('Pointer moved to {0}'.format((x, y)))
+
+    def on_click(self, x, y, button, pressed):
+        print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+        if not pressed:
+            # Stop listener
+            return False
+
+    def on_scroll(self, x, y, dx, dy):
+        print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up', (x, y)))
+
+
+if __name__ == '__main__':
+    mysensor = MouseSensor()
+    while True:
+        pass
