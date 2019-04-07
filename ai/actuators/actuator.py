dc194fb5265476c723df83d52bbb0dff791e2f56
Charles Hinson <cehinso17@gmail.com>
2019-04-07 22:28:30 +0800

    start of rewrite using decorators

--- ai/actuators/actuator.py
+++ ai/actuators/actuator.py
@@ -1,6 +1,33 @@
+import functools
+
+class ActionList:
+    '''Class that represents actions available to an acutator'''
+
+    def __init__(self):
+        self._actions = []
+
+    def __len__(self):
+        return len(self._actions)
+
+    def __getitem__(self, position):
+        return self._actions[position]
+
+    def register(self, func):
+        '''decorator that adds function to _actions'''
+        # FIXME using @wraps doesnt work
+        # @wraps(func)
+        # def wrapper(*args, **kwargs):
+        #     print("register")
+        #     return func(*args, **kwargs)
+        # return wrapper
+        print('register')
+        self._actions.append(func)
+        return func
+
 
 class Actuator:
     '''Base class for all actuators'''
 
-    def __init__(self, actions):
-        self.actions = actions
+    def __init__(self):
+        # TODO figure out how to refactor
+        pass
--- ai/actuators/keyboard_actuator.py
+++ ai/actuators/keyboard_actuator.py
@@ -1,37 +1,71 @@
 
-from actuators.actuator import Actuator
+from actuator import Actuator
+from actuator import ActionList
 
-from functools import partial
 import pyautogui
 import yaml
 
 # TODO pyautogui.press for attack keys, KeyDown and KeyUp for movement
+# TODO is there a difference between KeyDown + KeyUp and press?
+
 
 class KeyboardActuator(Actuator):
     '''Controls keyboard'''
+    actions = ActionList()
 
-    def __init__(self, keys):
+    def __init__(self, keys, allow_combos=False):
+        super().__init__()
         for key in keys:
             if key not in pyautogui.KEYBOARD_KEYS:
                 raise "Invalid Key"
         self.keys = keys
-        self.actions = [partial(pyautogui.press, key) for key in keys]
+        self._is_down = dict((key, False) for key in keys)
+        self.allow_combos = allow_combos
+        # TODO maybe add pyautogui.press as well
+        # self.actions = keys
+        # self.actions = [partial(pyautogui.keyDown, key) for key in keys] + \
+        #     [partial(pyautogui.keyUp, key) for key in keys]
 
     def to_file(self, filename):
+        # TODO save which type of keypress is called
         with open(filename, 'w') as outfile:
             yaml.dump(self.keys, outfile, default_flow_style=False)
 
-    @classmethod
+    @actions.register
     def from_file(cls, filename):
         with open(filename, 'r') as infile:
             keys = keys = yaml.load(infile)
             actuator = cls(keys)
             return actuator
 
+    @actions.register
+    def press(self, key):
+        pyautogui.press(key)
+
+    def is_down(self, key):
+        return self._is_down[key]
+
+    @actions.register
+    def down(self, key):
+        # FIXME cant press an already down key
+        if not self.allow_combos:
+            for k in self.keys:
+                if self._is_down[k]:
+                    self.up(k)
+        pyautogui.keyDown(key)
+
+    @actions.register
+    def up(self, key):
+        # FIXME cant lift an already up key
+        pyautogui.keyUp(key)
+
 
 if __name__ == '__main__':
 
     test_actuator = KeyboardActuator(['w', 'a', 's', 'd'])
-    test_actuator.to_file('keyfile.yaml')
-    test_actuator_2 = KeyboardActuator.from_file('keyfile.yaml')
-    print(test_actuator_2.keys)
+    # test_actuator.to_file('keyfile.yaml')
+    # test_actuator_2 = KeyboardActuator.from_file('keyfile.yaml')
+    # print(test_actuator_2.keys)
+    print(KeyboardActuator.actions._actions)
+    for action in KeyboardActuator.actions:
+        print(action)
