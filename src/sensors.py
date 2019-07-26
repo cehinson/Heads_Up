bb7a2a253f40e4d4ca4af57094ec8fde3aeb0457
Charles Hinson <cehinso17@gmail.com>
2019-07-26 11:13:51 +0800

    synchronize processes

--- src/sensors.py
+++ src/sensors.py
@@ -6,7 +6,7 @@ from collections import deque
 # Camera
 import multiprocessing
 # NOTE -- do not use the Queue from the queue module, it will lock...
-from multiprocessing import Queue, Process
+from multiprocessing import Queue, Process, Event
 import os
 import sys
 import numpy
@@ -83,19 +83,28 @@ class KeypressListener:
 
 class Camera:
     # TODO calculate the fps
-    def __init__(self, screen, out_dir='screenshots'):
-
+    def __init__(self, screen, pixel_ratio, out_dir='screenshots'):
+        self.out_dir = out_dir
         # ex: screen = {"top": 40, "left": 0, "width": 800, "height": 640}
         self.screen = screen
-        self.out_dir = out_dir
+        # deal with high-dpi screens
+        self.pixel_ratio = pixel_ratio
 
         # TODO this need to be updated in the area slect tool
         self.rects_to_segment = []
 
+        # ---------- Things for Multiprocessing ----------
+
         self._running = False
-        # it seems like maybe each function should have their own queues
+        # use a queue to share data between processes
         self.queue = Queue()
+        # pass the sentinel value into the queue to stop the processes
+        self._sentinel = object()
+        # synchronize the processes
+        self._event = Event()
+        # keep each separate process in this list
         self.processes = []
+        # TODO do not hardcode this
         self.funcs_to_run = [self.grab, self.ocr]
 
     def start(self):
@@ -108,47 +117,51 @@ class Camera:
             self.processes.append(process)
 
     def terminate(self):
+        # TODO can terminate a specific process
         self._running = False
         for process in self.processes:
             process.terminate()
 
     def grab(self):
-        '''
-        Grab a portion of the screen
-        '''
+        ''' Grab a portion of the screen '''
         # TODO add a way to limit the fps...
-        print('grabbing screen')
         while self._running:
             with mss.mss() as sct:
                 img = sct.grab(self.screen)
                 # FIXME I probably should not have to convert two times...
                 img = numpy.array(sct.grab(self.screen))
                 img = Image.fromarray(img)
-                # TODO should i move segmentation somewhere else?
+
                 if self.rects_to_segment:
                     for rect in self.rects_to_segment:
                         # FIXME the coords are wrong (only on laptop monitor) because of how QT does the screen size
                         coords = [rect.x(), rect.y(), rect.x()+rect.width(), rect.y()+rect.height()]
                         sub_img = img.crop(box=coords)
                         self.queue.put(sub_img)
+                        # wait for the consumer to process the image
+                        self._event.wait()
                 else:
                     self.queue.put(img)
-        self.queue.put(None)
+                    self._event.wait()
+
+        self.queue.put(self._sentinel)
 
     def ocr(self):
-        print('running ocr')
+        ''' Optical Character Recognition '''
         while True:
             img = self.queue.get()
 
-            if img is None:
+            if img is self._sentinel:
+                self.queue.put(self._sentinel)
                 break
 
             print(pytesseract.image_to_string(img))
+            # we have completed processing the image
+            self._event.set()
             sys.stdout.flush()  # FIXME this does not seem to work
 
     def save(self):
         ''' Save the images in the queue '''
-        print('saving')
         if not os.path.isdir(self.out_dir):
             os.mkdir(self.out_dir)
 
@@ -156,10 +169,13 @@ class Camera:
         number = 0
         output = self.out_dir + "/file_{}.png"
 
-        while True:  # "there are screenshots":
+        while True:
             img = self.queue.get()
-            if img is None:
+
+            if img is self._sentinel:
+                self.queue.put(self._sentinel)
                 break
 
             img.save(output.format(number))
+            self._event.set()
             number += 1
