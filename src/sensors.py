545810d7e5de7cdae14d9ad9db589c4ee7e2bec8
Charles Hinson <cehinso17@gmail.com>
2019-08-02 20:46:15 +0800

    removed some unnecessary subprocesses

--- src/sensors.py
+++ src/sensors.py
@@ -1,17 +1,15 @@
 
-from utils import clear_screen
+from utils import clear_screen, timeblock, timethis
 
 # Keyboard and Mouse Listeners
 from pynput import mouse, keyboard
 from collections import deque
 
 # Camera
-import multiprocessing as mp
-# NOTE -- do not use the Queue from the queue module, it will lock...
-from multiprocessing import Queue, Process, Event
+from multiprocessing import Process
 import os
-import sys
 import numpy
+import cv2
 from PIL import Image
 import mss
 import pytesseract
@@ -85,101 +83,89 @@ class KeypressListener:
 
 class Camera:
     # TODO calculate the fps
-    def __init__(self, screen, pixel_ratio, out_dir='screenshots'):
+    def __init__(self, screen, pixel_ratio, out_dir='camera'):
+        # save images to an output folder for logging / debugging
         self.out_dir = out_dir
+        if not os.path.isdir(self.out_dir):
+            os.mkdir(self.out_dir)
+        self.number = 0
+
         # ex: screen = {"top": 40, "left": 0, "width": 800, "height": 640}
         self.screen = screen
         # deal with high-dpi screens
         self.pixel_ratio = pixel_ratio
-        # TODO this need to be updated in the area slect tool
+
+        # NOTE updated in the area slect tool
         self.rects_to_segment = []
+
+        # make opencv run fast
+        if not cv2.useOptimized():
+            cv2.setUseOptimized(True)
+            assert(cv2.useOptimized() is True)
 
         # ---------- Things for Multiprocessing ----------
 
         self._running = False
-        # use a queue to share data between processes
-        self.queue = Queue()
-        # pass the sentinel value into the queue to stop the processes
-        self._sentinel = object()
-        # synchronize the processes
-        self._event = Event()
-        # keep each separate process in this list
-        self.processes = []
-        # TODO do not hardcode this
-        self.funcs_to_run = [self.grab, self.ocr]
+        self._process = None
 
     def start(self):
-        assert(len(self.funcs_to_run) < mp.cpu_count())
         self._running = True
-        for func in self.funcs_to_run:
-            # TODO add args
-            process = Process(target=func)
-            process.start()
-            self.processes.append(process)
+        # TODO add args
+        process = Process(target=self.grab)
+        process.start()
+        self._process = process
 
     def terminate(self):
         # TODO can terminate a specific process
         self._running = False
-        for process in self.processes:
-            process.terminate()
+        self._process.join()  # I think i need to do this to guarantee no zombies
+        self._process.terminate()
 
     def grab(self):
         ''' Grab a portion of the screen '''
         # TODO add a way to limit the fps...
         while self._running:
             with mss.mss() as sct:
-                # set event to false and block all other threads
-                self._event.clear()
-
-                img = sct.grab(self.screen)
-                # FIXME I probably should not have to convert two times...
+                # take screenshot of entire screen
                 img = numpy.array(sct.grab(self.screen))
-                img = Image.fromarray(img)
-                if self.rects_to_segment:
+                sub_imgs = []
+                # FIXME what if no sub images...
+                # segment all user labeled areas of the screen
+                # and group into subgroups for processing
+                with timeblock('segment {} '.format(len(self.rects_to_segment))):
                     for rect in self.rects_to_segment:
-                        # FIXME the coords are wrong (only on laptop monitor) because of how QT does the screen size
-                        coords = [rect.x(), rect.y(), rect.x()+rect.width(), rect.y()+rect.height()]
-                        sub_img = img.crop(box=coords)
-                        self.queue.put(sub_img)
-                        # wait for the consumer to process the image
-                else:
-                    self.queue.put(img)
+                        x, y, w, h = rect.x(), rect.y(), rect.width(), rect.height()
+                        # account for the difference in pixel coordinates in QT and numpy
+                        x = int(round(x * self.pixel_ratio))
+                        y = int(round(y * self.pixel_ratio))
+                        w = int(round(w * self.pixel_ratio))
+                        h = int(round(h * self.pixel_ratio))
+                        # NOTE opencv indexes with y first
+                        sub_img = img[y:y+h, x:x+w].copy()
+                        sub_imgs.append(sub_img)
 
-                self._event.wait()
+                # process the sub images...
+                with timeblock('ocr {} '.format(len(sub_imgs))):
+                    for img in sub_imgs:
+                        text = self.ocr(img)
+                        print(text)
 
-        self.queue.put(self._sentinel)
-
-    def ocr(self):
+    def ocr(self, img):
         ''' Optical Character Recognition '''
-        while True:
-            img = self.queue.get()
+        # convert to gray and threshold
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        # perform ocr
+        text = pytesseract.image_to_string(img) + ""
+        return text
 
-            if img is self._sentinel:
-                self.queue.put(self._sentinel)
-                break
-
-            # clear_screen()
-            print(pytesseract.image_to_string(img))
-            # we have completed processing the image
-            self._event.set()
-            # sys.stdout.flush()  # FIXME this does not seem to work
-
-    def save(self):
+    def save(self, img):
         ''' Save the images in the queue '''
-        if not os.path.isdir(self.out_dir):
-            os.mkdir(self.out_dir)
+        # TODO save based on which subimage they belong to
 
         # out file naming
-        number = 0
         output = self.out_dir + "/file_{}.png"
 
-        while True:
-            img = self.queue.get()
-
-            if img is self._sentinel:
-                self.queue.put(self._sentinel)
-                break
-
-            img.save(output.format(number))
-            self._event.set()
-            number += 1
+        img = Image.fromarray(img)
+        img.save(output.format(self.number))
+        self.number += 1
